dfc968d4e978a3c5ac68f84ac05f4943f57b74cb
Gerardo Alvarez <144278270+jesuslife@users.noreply.github.com>
2025-01-21 07:27:49 -0600

    substract-function added

--- calculator.py
+++ calculator.py
@@ -1,3 +1,16 @@
 """
 Write funcionalities
 """
+
+
+def subtract(a: int, b: int) -> int:
+    """Subtract two numbers
+
+    Args:
+        a (int): First number
+        b (int): Second number
+
+    Returns:
+        int: Result of the subtraction of a and b
+    """
+    return a - b
